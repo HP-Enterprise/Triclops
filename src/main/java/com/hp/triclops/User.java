ec4f77fe6662bbb0bba0a19480f1c43e40ec4a0e
jiangli <hyacinth0509@163.com>
2015-08-03 16:49:51 +0800

    新建User.java 对应Triclops的issues/1

--- src/main/java/com/hp/triclops/User.java
+++ src/main/java/com/hp/triclops/User.java
@@ -1,40 +1,105 @@
 package com.hp.triclops;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
-@Entity @Table(name="t_account")
+/**
+ * Created by liz on 2015/8/3.
+ */
+@Entity
+@Table(name = "t_user", schema = "", catalog = "briair")
 public class User {
+    private int id;
+    private String user;
+    private Integer gender;
+    private String nick;
+    private String phone;
+    private String password;
 
     @Id
-    @GeneratedValue(strategy=GenerationType.AUTO)
-    private long id;
-    private String name;
-    private String nick;
+    @Column(name = "Id", nullable = false, insertable = true, updatable = true)
+    public int getId() {
+        return id;
+    }
 
-    protected User() {}
+    public void setId(int id) {
+        this.id = id;
+    }
 
-    public User(String name, String nick) {
-        this.name = name;
+    @Basic
+    @Column(name = "user", nullable = false, insertable = true, updatable = true, length = 50)
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    @Basic
+    @Column(name = "gender", nullable = true, insertable = true, updatable = true)
+    public Integer getGender() {
+        return gender;
+    }
+
+    public void setGender(Integer gender) {
+        this.gender = gender;
+    }
+
+    @Basic
+    @Column(name = "nick", nullable = false, insertable = true, updatable = true, length = 50)
+    public String getNick() {
+        return nick;
+    }
+
+    public void setNick(String nick) {
         this.nick = nick;
     }
 
-    public String getName(){
-        return this.name;
+    @Basic
+    @Column(name = "phone", nullable = true, insertable = true, updatable = true, length = 11)
+    public String getPhone() {
+        return phone;
     }
 
-    public String getNick(){
-        return this.nick;
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    @Basic
+    @Column(name = "password", nullable = true, insertable = true, updatable = true, length = 100)
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 
     @Override
-    public String toString() {
-        return String.format(
-                "User[id=%d, name='%s', nick='%s']",
-                id, name, nick);
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user1 = (User) o;
+
+        if (id != user1.id) return false;
+        if (user != null ? !user.equals(user1.user) : user1.user != null) return false;
+        if (gender != null ? !gender.equals(user1.gender) : user1.gender != null) return false;
+        if (nick != null ? !nick.equals(user1.nick) : user1.nick != null) return false;
+        if (phone != null ? !phone.equals(user1.phone) : user1.phone != null) return false;
+        if (password != null ? !password.equals(user1.password) : user1.password != null) return false;
+
+        return true;
     }
 
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (user != null ? user.hashCode() : 0);
+        result = 31 * result + (gender != null ? gender.hashCode() : 0);
+        result = 31 * result + (nick != null ? nick.hashCode() : 0);
+        result = 31 * result + (phone != null ? phone.hashCode() : 0);
+        result = 31 * result + (password != null ? password.hashCode() : 0);
+        return result;
+    }
 }
