88526e60793b4001869573c19818b5349dc8e24c
jiangli <hyacinth0509@163.com>
2015-08-04 13:27:01 +0800

    修改#1 反馈问题

--- src/db/20150804A_user.sql
+++ src/db/20150804A_user.sql
@@ -3,7 +3,6 @@ CREATE TABLE t_user (
   name varchar(50) NOT NULL DEFAULT '' COMMENT '账号',
   gender int(3) DEFAULT NULL COMMENT '性别',
   nick varchar(50) NOT NULL DEFAULT '' COMMENT '显示给其它人看的名称',
-  phone varchar(11) DEFAULT NULL COMMENT '电话号码',
-  password varchar(100) DEFAULT '' COMMENT '密码',
-  PRIMARY KEY ('Id')
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT COMMENT='用户表';
+  phone varchar(11) DEFAULT NULL COMMENT '电话号码'
+  PRIMARY KEY (Id)
+) DEFAULT CHARSET=utf8 COMMENT='用户表';
