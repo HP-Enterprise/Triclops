21191064176b103a3ee34adaaba27fee027b0bb7
jefferyxq <jeffary8110@hotmail.com>
2015-08-17 23:34:32 +0800

    新增redis注入

--- src/main/java/com/hp/triclops/SimpleConfig.java
+++ src/main/java/com/hp/triclops/SimpleConfig.java
@@ -1,8 +1,31 @@
 package com.hp.triclops;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Component;
+import redis.clients.jedis.JedisPoolConfig;
 
+@Component
 @Configuration
-@EnableAutoConfiguration
-class SimpleConfig {}
+public class SimpleConfig {
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory(){
+        JedisPoolConfig poolConfig = new JedisPoolConfig();
+        poolConfig.setMaxTotal(10000);
+        poolConfig.setMinIdle(1000);
+        poolConfig.setMaxIdle(-1);
+        poolConfig.setMaxWaitMillis(500);
+        poolConfig.setTestOnBorrow(true);
+        poolConfig.setTestOnReturn(true);
+
+        return new JedisConnectionFactory(poolConfig);
+    }
+
+    @Bean
+    public StringRedisTemplate stringRedisTemplate(){
+        return new StringRedisTemplate(redisConnectionFactory());
+    }
+}
