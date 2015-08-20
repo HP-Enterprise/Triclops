388c06f9032c58f100c6bc28b90b45fc2cfabb45
wh <haowang@winphone.us>
2015-08-20 09:15:49 +0800

    user查找

--- src/main/java/com/hp/triclops/repository/UserRepository.java
+++ src/main/java/com/hp/triclops/repository/UserRepository.java
@@ -4,8 +4,14 @@ import java.util.List;
 
 import com.hp.triclops.entity.User;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends CrudRepository<User, Long> {
 
     List<User> findByNick(String nick);
+
+    User findByName(String name);
+
+    User findById(int id);
 }
