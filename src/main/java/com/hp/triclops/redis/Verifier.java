b656c5e87dd2de2c7e5e96bc74aa3a451d336aa1
shuzihao <zhshu@incardata.com.cn>
2015-08-21 10:06:17 +0800

    完成Verifier类生成与校验验证码功能

--- src/main/java/com/hp/triclops/redis/Verifier.java
+++ src/main/java/com/hp/triclops/redis/Verifier.java
@@ -1,21 +1,37 @@
 package com.hp.triclops.redis;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+import java.util.Random;
 
 /**
  * 校验器
  * 产生临时性的随机校验码
  * 然后进行一致性校验
  */
+@Component
 public class Verifier {
+    @Autowired
+    SessionRedis sessionRedis;
     /**
      * 产生一个6位的纯数字的随机校验码
-     * @param 校验码目标
-     * @param 过期时间(秒)
+     * @param target 校验码目标
+     * @param expiredSeconds 过期时间(秒)
      * @return 成功返回纯数字的6位校验码
      */
     public String generateCode(String target, int expiredSeconds){
-        throw new NotImplementedException();
+        StringBuffer buffer = new StringBuffer("0123456789");
+        StringBuffer saltStr = new StringBuffer();
+
+        Random random = new Random();
+        int range = buffer.length();
+        for(int i = 0;i < 6;i++){
+            saltStr.append(buffer.charAt(random.nextInt(range)));
+        }
+        sessionRedis.saveSessionOfVal(target,saltStr.toString(),expiredSeconds);
+        return saltStr.toString();
     }
 
     /**
@@ -25,6 +41,14 @@ public class Verifier {
      * @return 成功返回TRUE, 失败返回FALSE
      */
     public boolean verifyCode(String target, String code){
-        throw new NotImplementedException();
+        boolean result=false;
+        String trueCode=sessionRedis.getSessionOfVal(target);
+        if (trueCode!=null)
+        {
+            if(trueCode .equals(code)){
+                result=true;
+            }
+        }
+        return result;
     }
 }
