4d422bc4a1665314f7a8fab091bef5a08a104f18
jiangli <hyacinth0509@163.com>
2015-08-26 19:21:08 +0800

    字段名写错了

--- src/main/resources/db/migration/V20150804.1__user.sql
+++ src/main/resources/db/migration/V20150804.1__user.sql
@@ -4,6 +4,6 @@ CREATE TABLE t_user (
   gender int(3) DEFAULT NULL COMMENT '性别',
   nick varchar(50) NOT NULL DEFAULT '' COMMENT '显示给其它人看的名称',
   phone varchar(11) DEFAULT NULL COMMENT '电话号码',
-  isVerified int(1) DEFAULT 0 COMMENT '0表示未验证，1表示已验证',
+  is_verified  int(1) DEFAULT 0 COMMENT '0表示未验证，1表示已验证',
   PRIMARY KEY (Id)
 ) DEFAULT CHARSET=utf8 COMMENT='用户表';
