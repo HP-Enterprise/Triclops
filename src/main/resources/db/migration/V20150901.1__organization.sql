18908f5bbd2227b229f6717acb973e82f5135ba2
shebing <958367751@qq.com>
2015-09-02 11:05:02 +0800

    修改主键

--- src/main/resources/db/migration/V20150901.1__organization.sql
+++ src/main/resources/db/migration/V20150901.1__organization.sql
@@ -5,5 +5,5 @@ CREATE TABLE t_organization (
   type_key int(1) NOT NULL COMMENT '组织类型',
   sub_oid int(11) NOT NULL COMMENT '子组织id',
   par_oid int(11) NOT NULL COMMENT '父组织id',
-  PRIMARY KEY (O_Id)
+  PRIMARY KEY (Id)
 ) DEFAULT CHARSET=utf8 COMMENT='组织表';
