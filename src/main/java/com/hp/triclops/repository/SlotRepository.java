676ce1f16227df4d259b907f4c787d420355e17b
jack-luj <jlu@incardata.com.cn>
2015-09-02 17:47:38 +0800

    改进查询通讯录keys的方法

--- src/main/java/com/hp/triclops/repository/SlotRepository.java
+++ src/main/java/com/hp/triclops/repository/SlotRepository.java
@@ -3,8 +3,11 @@ package com.hp.triclops.repository;
 import com.hp.triclops.entity.SlotInfo;
 import com.hp.triclops.entity.User;
 import com.hp.triclops.entity.Vehicle;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
 
 /**
  * Created by Administrator on 2015/8/25.
@@ -17,5 +20,9 @@ public interface SlotRepository extends CrudRepository<SlotInfo, Long> {
 
     SlotInfo findByUidAndSlotkey(String uid,String slotkey);
 
-    Iterable<SlotInfo> findByUidAndSlotkeyStartingWith(String uid,String slotkey);
+    @Query("select s.slotkey from SlotInfo s where s.uid=?1 And s.slotkey like ?2")
+    List<String> findKeysByUidAndSlotkeyStartingWith(String uid,String slotkey);
+
+    @Query("select s.slotkey from SlotInfo s where s.uid=?1")
+    List<String> findKeysByUid(String uid);
 }
