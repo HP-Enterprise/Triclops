e99831333109d9e5a7e360068d095e5b2e67c1b3
shebing <958367751@qq.com>
2015-09-02 15:23:21 +0800

    修改组织表的名称

--- src/main/resources/db/migration/V20150902.1__organizationRelatived.sql
+++ src/main/resources/db/migration/V20150902.1__organizationRelatived.sql
@@ -1,4 +1,4 @@
-CREATE TABLE t_organizationRelatived (
+CREATE TABLE t_organizationrelatived (
   Id int(11) NOT NULL AUTO_INCREMENT COMMENT '关系id',
   sub_oid int(11) NOT NULL COMMENT '子组织id',
   par_oid int(11) NOT NULL COMMENT '父组织id',
