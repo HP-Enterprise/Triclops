a25c595fa83831396fb6e68965d39f9ba6421b35
teemol <teemol@163.com>
2015-09-06 11:30:07 +0800

    新增车主车辆关系实体类及查询方法

--- src/main/java/com/hp/triclops/repository/UserVehicleRelativedRepository.java
+++ src/main/java/com/hp/triclops/repository/UserVehicleRelativedRepository.java
@@ -1,7 +1,11 @@
 package com.hp.triclops.repository;
 
+import com.hp.triclops.entity.UserVehicleRelatived;
+import org.springframework.data.repository.CrudRepository;
+
 /**
  * Created by Teemol on 2015/9/5.
  */
-public interface UserVehicleRelativedRepository {
+public interface UserVehicleRelativedRepository extends CrudRepository<UserVehicleRelatived, String> {
+
 }
