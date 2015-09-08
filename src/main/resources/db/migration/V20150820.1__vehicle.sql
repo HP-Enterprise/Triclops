6d9f844f46183b4dfcec5c7e1f1d72d6b9ec8a5c
jiangli <hyacinth0509@163.com>
2015-09-08 17:45:17 +0800

    去掉一个重复的建表t_user_vehicle_relatived

--- src/main/resources/db/migration/V20150820.1__vehicle.sql
+++ src/main/resources/db/migration/V20150820.1__vehicle.sql
@@ -19,12 +19,4 @@ create table sysdict
   PRIMARY KEY (dictid)
 ) DEFAULT CHARSET=utf8 COMMENT='字典表';
 
-create table t_user_vehicle_relatived(
-  id int(11) NOT NULL AUTO_INCREMENT COMMENT '车辆绑定id',
-  userid int not null COMMENT '用户ID',
-  vin varchar(30) not null COMMENT '车辆vin码',
-  iflag int not null COMMENT '车主标识 1 车主 0 朋友' ,
-  parent_user_id int COMMENT '车主id',
-  PRIMARY KEY (id)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COMMENT='车辆绑定关系表';
 
