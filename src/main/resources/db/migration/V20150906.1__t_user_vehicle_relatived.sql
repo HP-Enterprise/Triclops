7ed22ec8c819d4f3c680f54d4d585560238833c2
jiangli <hyacinth0509@163.com>
2015-09-09 14:21:00 +0800

    修改表结构

--- src/main/resources/db/migration/V20150906.1__t_user_vehicle_relatived.sql
+++ src/main/resources/db/migration/V20150906.1__t_user_vehicle_relatived.sql
@@ -4,9 +4,5 @@ create table t_user_vehicle_relatived(
   vin varchar(30) not null COMMENT '车辆vin码',
   iflag int not null COMMENT '车主标识 1 车主 0 朋友' ,
   parent_user_id int COMMENT '车主id',
-  PRIMARY KEY (id),
-  CONSTRAINT FOREIGN KEY (userid) REFERENCES t_user(Id)
-  ON DELETE RESTRICT ON UPDATE CASCADE,
-  CONSTRAINT FOREIGN KEY (vin) REFERENCES t_vehicle(vin)
-  ON DELETE RESTRICT ON UPDATE CASCADE
-)DEFAULT CHARSET=utf8 COMMENT='车辆绑定关系表';
+  PRIMARY KEY (id)
+) DEFAULT CHARSET=utf8 COMMENT='车辆绑定关系表';
