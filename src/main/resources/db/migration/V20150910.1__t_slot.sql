92f4107f9b5eecece6cb06d3b4f4222c67a656c1
jack-luj <jlu@incardata.com.cn>
2015-09-10 17:46:31 +0800

    修改通讯录实体uid为int类型

---
+++ src/main/resources/db/migration/V20150910.1__t_slot.sql
@@ -0,0 +1,8 @@
+﻿drop table if exists  t_slot;
+CREATE TABLE t_slot (
+  id bigint(12) NOT NULL AUTO_INCREMENT COMMENT 'id',
+  uid int(11) NOT NULL COMMENT '用户id',
+  slotkey varchar(50) NOT NULL COMMENT 'slotkey',
+  slot longblob NOT NULL COMMENT 'slot二进制数据',
+  PRIMARY KEY (id)
+)  DEFAULT CHARSET=utf8 COMMENT='通讯录表';
