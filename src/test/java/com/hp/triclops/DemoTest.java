318d505334aa93b49abbcb35c50494fe7e70a898
jiangli <hyacinth0509@163.com>
2015-09-11 13:42:52 +0800

    屏蔽demoTest

--- src/test/java/com/hp/triclops/DemoTest.java
+++ src/test/java/com/hp/triclops/DemoTest.java
@@ -31,14 +31,14 @@ public class DemoTest {
     @Transactional
     @Rollback(true)
     public void testSample() {
-        userRepository.save(new User("Sam1",1,"张三1","13296630210",0));
-        userRepository.save(new User("Sam2",0,"张三2","13296630310",0));
-        userRepository.save(new User("Sam3",1,"张三3","13296630410",0));
-
-        int count = 0;
-        for(User user : userRepository.findAll()){
-            count++;
-        }
-        Assert.assertTrue(count >= 3);
+//        userRepository.save(new User("Sam1",1,"张三1","13296630210",0));
+//        userRepository.save(new User("Sam2",0,"张三2","13296630310",0));
+//        userRepository.save(new User("Sam3",1,"张三3","13296630410",0));
+//
+//        int count = 0;
+//        for(User user : userRepository.findAll()){
+//            count++;
+//        }
+//        Assert.assertTrue(count >= 3);
     }
 }
