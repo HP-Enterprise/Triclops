807b57c04a26b86150b273fb7208c6a98489c4c7
jiangli <hyacinth0509@163.com>
2015-09-12 15:02:52 +0800

    建立索引

--- src/main/resources/db/migration/V20150901.2__organization.sql
+++ src/main/resources/db/migration/V20150901.2__organization.sql
@@ -1,7 +1,11 @@
-CREATE TABLE t_organization (
+CREATE TABLE IF NOT EXISTS t_organization (
   Id int(11) NOT NULL AUTO_INCREMENT COMMENT '组织的id',
   org_name varchar(128) NOT NULL COMMENT '组织名称',
   bre_code varchar(8) UNIQUE NOT NULL COMMENT '组织简码',
   type_key int(1) NOT NULL COMMENT '组织类型',
-  PRIMARY KEY (Id)
+  PRIMARY KEY (Id),
+  UNIQUE KEY (org_name),
+  KEY idx_typekey (type_key)
 ) DEFAULT CHARSET=utf8 COMMENT='组织表';
+
+INSERT INTO  t_organization(org_name, bre_code, type_key) VALUES ("超级管理后台","SUPPER",1)
--- src/main/resources/db/migration/V20150902.2__organization_relatived.sql
+++ src/main/resources/db/migration/V20150902.2__organization_relatived.sql
@@ -1,6 +1,8 @@
-CREATE TABLE t_organization_relatived (
+CREATE TABLE IF NOT EXISTS t_organization_relatived (
   Id int(11) NOT NULL AUTO_INCREMENT COMMENT '关系id',
   sub_oid int(11) NOT NULL COMMENT '子组织id',
   par_oid int(11) NOT NULL COMMENT '父组织id',
-  PRIMARY KEY (Id)
+  PRIMARY KEY (Id),
+  FOREIGN KEY (sub_oid) REFERENCES t_organization(Id),
+  FOREIGN KEY (par_oid) REFERENCES t_organization(Id)
 ) DEFAULT CHARSET=utf8 COMMENT='组织关系表';
