e5d2644c791006d8c777c328bcb676a81770df90
tianye <ytian@incardata.com.cn>
2015-09-14 14:22:59 +0800

    修改sql，添加默认值和修改注释

--- src/main/resources/db/migration/V20150820.1__vehicle.sql
+++ src/main/resources/db/migration/V20150820.1__vehicle.sql
@@ -4,10 +4,10 @@ create table t_vehicle
   vin varchar(50) NOT NULL COMMENT '车架号',
   vendor varchar(100) COMMENT '厂家',
   model varchar(100) COMMENT '型号',
-  t_flag int COMMENT '--0 自然吸气 1 涡轮增压',
+  t_flag int COMMENT '--0 车辆未禁用 1 已禁用',
   displacement varchar(20) COMMENT '排量',
   license_plate varchar(10) COMMENT '车牌号',
-  product_date datetime COMMENT '生产日期',
+  product_date datetime DEFAULT '1970-01-01 00:00:00' NOT NULL COMMENT '生产日期',
   PRIMARY KEY (id)
 ) DEFAULT CHARSET=utf8 COMMENT='车辆表';
 
