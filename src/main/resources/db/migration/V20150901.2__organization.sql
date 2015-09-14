7197e9a39adb376d950ec6e43cc2f249a5d9e352
jiangli <hyacinth0509@163.com>
2015-09-14 12:24:06 +0800

    修复sqlbug

--- src/main/resources/db/migration/V20150901.2__organization.sql
+++ src/main/resources/db/migration/V20150901.2__organization.sql
@@ -4,8 +4,8 @@ CREATE TABLE IF NOT EXISTS t_organization (
   bre_code varchar(8) UNIQUE NOT NULL COMMENT '组织简码',
   type_key int(3) NOT NULL COMMENT '组织类型，1是华晨汽车，2是经销商，3是车友群',
   descript VARCHAR(100) COMMENT '描述',
-  available int(1) NOT NULL COMMENT '是否可用，0失效，1可用',
-  PRIMARY KEY (Id)PRIMARY KEY (Id),
+  available int(1) NOT NULL DEFAULT 1 COMMENT '是否可用，0失效，1可用',
+  PRIMARY KEY (Id),
   UNIQUE KEY (org_name),
   KEY idx_typekey (type_key)
 ) DEFAULT CHARSET=utf8 COMMENT='组织表';
