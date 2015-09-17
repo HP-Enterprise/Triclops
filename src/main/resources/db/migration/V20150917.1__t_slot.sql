c58a11c0da4860e2f3e54894f1b733b18439ee68
jack-luj <jlu@incardata.com.cn>
2015-09-17 09:39:08 +0800

    删除旧的sql脚本，提交新版本sql

--- src/main/resources/db/migration/V20150917.1__t_slot.sql
+++ src/main/resources/db/migration/V20150917.1__t_slot.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS t_slot (
   id bigint(12) NOT NULL AUTO_INCREMENT COMMENT 'id',
   uid int(11) NOT NULL COMMENT '用户id',
   slotkey varchar(50) NOT NULL COMMENT 'slotkey',
-  slot longblob NOT NULL COMMENT 'slot二进制数据',
+  slot longtext NOT NULL COMMENT 'slot数据base64',
   PRIMARY KEY (id),
   FOREIGN KEY (uid) REFERENCES t_user(Id)
 ) DEFAULT CHARSET=utf8 COMMENT='通讯录表';
