dab7741b5e0dc7ab6e64b2f51fa123a5ca297fe7
jack-luj <jlu@incardata.com.cn>
2015-10-08 10:28:25 +0800

    repository数据库接口建立

--- src/main/java/com/hp/triclops/repository/WarningMessageDataRespository.java
+++ src/main/java/com/hp/triclops/repository/WarningMessageDataRespository.java
@@ -1,7 +1,10 @@
 package com.hp.triclops.repository;
 
+import com.hp.triclops.entity.WarningMessageData;
+import org.springframework.data.repository.CrudRepository;
+
 /**
  * Created by luj on 2015/10/8.
  */
-public interface WarningMessageDataRespository {
+public interface WarningMessageDataRespository extends CrudRepository<WarningMessageData, Long> {
 }
