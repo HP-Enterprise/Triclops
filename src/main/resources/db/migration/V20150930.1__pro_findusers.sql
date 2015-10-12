11a349f9ca06a5c5c97f0aa049b366c14ccf0d60
jiangli <hyacinth0509@163.com>
2015-10-12 20:17:17 +0800

    修改存储过程的bug

---
+++ src/main/resources/db/migration/V20150930.1__pro_findusers.sql
@@ -0,0 +1,46 @@
+DROP PROCEDURE IF EXISTS pro_findusers;
+CREATE PROCEDURE pro_findusers(vid int, isowner int, uid int,gender int, isVerified int, fuzzy int, nickname VARCHAR(50),firstRcord int, pageSize int, orderByProperty VARCHAR(15), ascOrDesc VARCHAR(5))
+BEGIN
+		CREATE TEMPORARY table  the_users
+		(
+			Id INT PRIMARY KEY,
+			name VARCHAR(50),
+			gender INT(3),
+			nick VARCHAR(50),
+			phone VARCHAR(11),
+			is_verified INT(1),
+      UNIQUE KEY unique_name (name),
+      KEY idx_phone (phone)
+		) ENGINE = MEMORY DEFAULT CHARSET=utf8;
+			INSERT INTO the_users SELECT DISTINCT U.* FROM t_user U LEFT JOIN t_organization_user OU ON u.Id =  OU.uid WHERE OU.oid in
+             (SELECT O.Id FROM t_organization O
+                 LEFT JOIN t_authoritygroup AG ON O.Id = AG.oid
+                 LEFT JOIN t_authoritygroup_relatived AGR ON AG.id = AGR.ag_id 
+                 LEFT JOIN t_authoritygroup_user AGU ON AGR.ag_id = AGU.ag_id WHERE AGR.a_id = 0 AND AGU.u_id = uid);
+            SET @vid = vid;
+            SET @isowner = isowner;
+            SET @gender = gender;
+            SET @isVerified = isVerified;
+            SET @nickname = nickname;
+            SET @firstRcord = firstRcord;
+            SET @pageSize = pageSize;
+            SET @orderByProperty = orderByProperty;
+            SET @ascOrDesc = ascOrDesc;
+            SET @sql = "SELECT * FROM the_users u";
+            SET @sql = CONCAT(@sql, " LEFT JOIN t_user_vehicle_relatived uvr ON u.id = uvr.userid WHERE (@vid = 0 OR uvr.vid = ?) AND (@isowner = -1 OR uvr.iflag = ?)");
+            SET @sql = CONCAT(@sql, " AND (@gender = -1 OR u.gender = ?)");
+            SET @sql = CONCAT(@sql, " AND (@isVerified = -1 OR u.is_verified = ?)");
+            IF FUZZY = 1 THEN     
+                 SET @sql = CONCAT(@sql, " AND (@nickname is null OR u.nick like ?)");
+            ELSE             
+                 SET @sql = CONCAT(@sql, " AND (@nickname is null OR u.nick = ?)");
+            END IF;
+
+            SET @sql = CONCAT(@sql, " ORDER BY u.",@orderByProperty," ",@ascOrDesc);
+             IF @firstRcord != -1 AND @pageSize != -1 THEN            
+                SET @sql = CONCAT(@sql, " limit ", @firstRcord, "," ,@pageSize);
+            END IF;
+            PREPARE sqlstr from @sql;
+            EXECUTE sqlstr using @vid,@isowner,@gender,@isVerified,@nickname;
+   		    DROP TABLE the_users;
+END;
