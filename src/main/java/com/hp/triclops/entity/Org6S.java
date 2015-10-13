0d512ffc9bd22dfa25f3241012a9225ea84b1b5d
jiangli <hyacinth0509@163.com>
2015-10-13 10:20:05 +0800

    设计组织与车辆的对象

--- src/main/java/com/hp/triclops/entity/Org6S.java
+++ src/main/java/com/hp/triclops/entity/Org6S.java
@@ -1,7 +1,54 @@
 package com.hp.triclops.entity;
 
+import com.hp.triclops.repository.OrganizationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Set;
+
 /**
  * Created by liz on 2015/10/13.
  */
+@Component
 public class Org6S {
+
+    @Autowired
+    private OrganizationRepository organizationRepository;
+
+    private Organization organization;
+
+    /**
+     * 构造函数
+     * @param oid
+     */
+    public Org6S(int oid){
+       this.setOrganization(this.findOrgById(oid));
+    }
+
+    /**
+     * 向组织里面添加一台车
+     * @param v
+     */
+    public void Add(Vehicle6S v){
+        Set<Vehicle> vehicleSet = this.organization.getVehicleSet();
+        vehicleSet.add(v.getVehicle());
+        organizationRepository.save(this.organization);
+    }
+
+    /**
+     * 根据组织ID查找组织
+     * @param oid
+     * @return
+     */
+    public Organization findOrgById(int oid){
+        return organizationRepository.findById(oid);
+    }
+
+    public Organization getOrganization() {
+        return organization;
+    }
+
+    public void setOrganization(Organization organization) {
+        this.organization = organization;
+    }
 }
--- src/main/java/com/hp/triclops/entity/Vehicle6S.java
+++ src/main/java/com/hp/triclops/entity/Vehicle6S.java
@@ -1,7 +1,46 @@
 package com.hp.triclops.entity;
 
+import com.hp.triclops.repository.VehicleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 /**
  * Created by liz on 2015/10/13.
  */
-public class Vehicle6S {
+@Component
+public class Vehicle6S{
+
+    @Autowired
+    private VehicleRepository vehicleRepository;
+
+    private Vehicle vehicle;
+
+
+    /**
+     * 构造函数
+     * @param vid
+     */
+
+    public Vehicle6S(int vid){
+         this.setVehicle(this.findVehicleById(vid));
+    }
+
+    /**
+     * 根据车辆ID查找车辆信息
+     * @param vid
+     * @return
+     */
+    public Vehicle findVehicleById(int vid){
+        return vehicleRepository.findById(vid);
+    }
+
+
+    public Vehicle getVehicle() {
+        return vehicle;
+    }
+
+    public void setVehicle(Vehicle vehicle) {
+        this.vehicle = vehicle;
+    }
+
 }
