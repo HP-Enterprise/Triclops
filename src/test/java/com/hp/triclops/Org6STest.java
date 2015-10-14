7032b5f664b17445610d5f252d6ec44915e973fb
wangh2015 <hwang2@incardata.com.cn>
2015-10-14 11:45:09 +0800

    新增组织绑定车辆方法，及测试方法

--- src/test/java/com/hp/triclops/Org6STest.java
+++ src/test/java/com/hp/triclops/Org6STest.java
@@ -1,7 +1,9 @@
 package com.hp.triclops;
 
 import com.hp.triclops.entity.Org6S;
+import com.hp.triclops.entity.Vehicle;
 import com.hp.triclops.entity.Vehicle6S;
+import com.hp.triclops.repository.VehicleRepository;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -10,12 +12,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import javax.transaction.Transactional;
+import java.util.Date;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = Application.class)
 public class Org6STest {
     @Autowired
     private ApplicationContext appContext;
+
+    @Autowired
+    VehicleRepository vehicleRepository;
 
 
     @Before
@@ -27,10 +34,22 @@ public class Org6STest {
     }
 
     @Test
+    @Transactional
     public void testAddVehivle() {
-      //  Org6S org6S=new Org6S(1);
-   //     org6S.setAppCtxAndInit(appContext);
-        Vehicle6S vehicle6S=new Vehicle6S(3);
+
+        Org6S org6S=new Org6S(1);
+        org6S.setAppCtxAndInit(appContext);
+        //新建车辆
+        Vehicle vehicle=new Vehicle();
+        vehicle.setVin("123321");
+        vehicle.setTboxsn("123321123");
+        vehicle.setLicense_plate("ABC123");
+        vehicle.setProduct_date(new Date());
+        //保存车辆
+        Vehicle testVehicle=vehicleRepository.save(vehicle);
+
+        Vehicle6S vehicle6S=new Vehicle6S(testVehicle.getId());
         vehicle6S.setAppCtxAndInit(appContext);
+        org6S.addVehicle(vehicle6S);
     }
 }
