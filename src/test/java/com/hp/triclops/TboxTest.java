c58c5547aa6fd03c3fb6cdb44af6236727ba8822
wangh2015 <hwang2@incardata.com.cn>
2015-10-23 16:49:16 +0800

    解决报错

--- src/test/java/com/hp/triclops/TboxTest.java
+++ src/test/java/com/hp/triclops/TboxTest.java
@@ -16,7 +16,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = Application.class)
-public class TBoxTest {
+public class TboxTest {
 
     @Autowired
     ApplicationContext appContext;
