6d62104576ba01bdc3b38ae61b756ca3442949e7
jiangli <hyacinth0509@163.com>
2015-10-27 10:25:05 +0800

    绑定TBox层

--- src/main/java/com/hp/triclops/manager/Vehicle7.java
+++ src/main/java/com/hp/triclops/manager/Vehicle7.java
@@ -1,7 +1,23 @@
 package com.hp.triclops.manager;
 
+import com.hp.triclops.entity.TBox;
+import com.hp.triclops.entity.Vehicle;
+
 /**
  * Created by liz on 2015/10/26.
  */
-public class Vehicle7 {
+public class Vehicle7 extends Vehicle6S {
+
+    public Vehicle7(){
+
+    }
+
+    /**
+     * 绑定TBox
+     * @param tBox
+     * @return
+     */
+    public Vehicle bindTbox(TBox tBox){
+
+    }
 }
