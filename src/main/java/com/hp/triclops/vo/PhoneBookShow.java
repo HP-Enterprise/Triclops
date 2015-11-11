3b43623f8fc3921394d2e03643c60f0f48b0ebbb
wyang181300 <ywang@incardata.com.cn>
2015-11-11 17:52:21 +0800

    +在'com.hp.triclops.vo.PhoneBookShow'类中添加构造器 +通过继承'PhoneBook'类,调用父类的构造器初始化此类对象

--- src/main/java/com/hp/triclops/vo/PhoneBookShow.java
+++ src/main/java/com/hp/triclops/vo/PhoneBookShow.java
@@ -1,13 +1,19 @@
 package com.hp.triclops.vo;
 
+import com.hp.triclops.entity.PhoneBook;
+
 /**
  * Created by Teemol on 2015/11/11.
  */
-public class PhoneBookShow {
+public class PhoneBookShow extends PhoneBook {
 
     private int id;
     private int uid;
     private String name;
     private String phone;
     private int isuser;
+
+    public PhoneBookShow(int id, int uid, String name, String phone, int isuser) {
+        super(id, uid, name, phone, isuser);
+    }
 }
