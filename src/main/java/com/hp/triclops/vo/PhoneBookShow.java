417d692b4e37cfffca35a6dbafaacb4e2d01f5ac
wyang181300 <ywang@incardata.com.cn>
2015-11-11 18:08:40 +0800

    +在'com.hp.triclops.vo.PhoneBookShow'类 +增加一个构造方法，用'com.hp.triclops.entity.PhoneBook'实体类对象初始化此类对象

--- src/main/java/com/hp/triclops/vo/PhoneBookShow.java
+++ src/main/java/com/hp/triclops/vo/PhoneBookShow.java
@@ -5,15 +5,70 @@ import com.hp.triclops.entity.PhoneBook;
 /**
  * Created by Teemol on 2015/11/11.
  */
-public class PhoneBookShow extends PhoneBook {
+public class PhoneBookShow{
 
     private int id;
     private int uid;
     private String name;
     private String phone;
     private int isuser;
+    public PhoneBookShow(){
 
-    public PhoneBookShow(int id, int uid, String name, String phone, int isuser) {
-        super(id, uid, name, phone, isuser);
+    }
+
+    public PhoneBookShow(int id,int uid,String name,String phone,int isuser){
+        this.id = id;
+        this.uid = uid;
+        this.name = name;
+        this.phone = phone;
+        this.isuser = isuser;
+    }
+
+    public PhoneBookShow(PhoneBook phoneBook){
+        this.id = phoneBook.getId();
+        this.uid = phoneBook.getUid();
+        this.name = phoneBook.getName();
+        this.phone = phoneBook.getPhone();
+        this.isuser = phoneBook.getIsuser();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getUid() {
+        return uid;
+    }
+
+    public void setUid(int uid) {
+        this.uid = uid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public int getIsuser() {
+        return isuser;
+    }
+
+    public void setIsuser(int isuser) {
+        this.isuser = isuser;
     }
 }
