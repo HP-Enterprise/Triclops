3efce4fd51d50a40e88bb184e12375734de23040
tianye <ytian@incardata.com.cn>
2015-11-17 14:59:56 +0800

    切面编程

--- src/main/java/com/hp/triclops/utils/RepositoryTAop.java
+++ src/main/java/com/hp/triclops/utils/RepositoryTAop.java
@@ -1,7 +1,29 @@
 package com.hp.triclops.utils;
 
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.context.annotation.Configuration;
+
 /**
  * Created by samsung on 2015/11/17.
  */
+@Aspect
+@Configuration
 public class RepositoryTAop {
+
+
+    @Pointcut("execution(* com.hp.*.repository.*.*(..))")
+    public void excudeController(){}
+
+    /**
+     * 调用repository里面的类之前执行
+     * @param joinPoint
+     * 该方法要将repository里面类的调用者，URL全路径，方法名，参数列表，调用时间写入到log日志中
+     */
+    @Before(value = "excudeController()")
+    public void BeforeMethodRun(JoinPoint joinPoint){
+        System.out.println("----------------------");
+    }
 }
