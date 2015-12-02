c5d7d21bdaef4dbeb554234a13468a2a5585854b
TeemolSparrow <mtian@incardata.com.cn>
2015-12-02 18:31:24 +0800

    修改方法

--- src/main/java/com/hp/triclops/vehicle/VehicleManagement.java
+++ src/main/java/com/hp/triclops/vehicle/VehicleManagement.java
@@ -1,18 +1,9 @@
 package com.hp.triclops.vehicle;
 
-import com.hp.triclops.entity.User;
-import com.hp.triclops.entity.UserVehicleRelatived;
 import com.hp.triclops.entity.Vehicle;
-import com.hp.triclops.repository.UserRepository;
-import com.hp.triclops.repository.UserVehicleRelativedRepository;
 import com.hp.triclops.repository.VehicleRepository;
-import com.hp.triclops.vo.UserVehicleRelativedShow;
-import com.hp.triclops.vo.VehicleShow;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Created by Teemol on 2015/11/11.
@@ -20,8 +11,6 @@ import java.util.List;
 @Component
 public class VehicleManagement {
 
-    @Autowired
-    UserVehicleRelativedRepository userVehicleRelativedRepository;
     @Autowired
     VehicleRepository vehicleRepository;
 
@@ -33,25 +22,4 @@ public class VehicleManagement {
     public Vehicle findById(int vid){
         return vehicleRepository.findById(vid);
     }
-
-
-    /**
-     * 查询用户的默认车辆
-     * @param uid 用户ID
-     * @return 车辆信息
-     */
-    public VehicleShow getDefaultCar(int uid){
-        User user = new User();
-        user.setId(uid);
-        VehicleShow vehicleShow = null;
-        List<UserVehicleRelatived> userVehicleRelativedList = userVehicleRelativedRepository.findByUid(user);
-        for (UserVehicleRelatived uv : userVehicleRelativedList) {
-            if (uv.getVflag() == 1) {
-                vehicleShow = new VehicleShow(uv.getVid(),uv.getVflag());
-                break;
-            }
-        }
-        return vehicleShow;
-    }
-    
 }
