40569620bcaad888b4f2736493e178b7f3fb6cdf
ZaiHang <wnddt0717@163.com>
2015-12-26 09:44:02 +0800

    INDEX

--- src/main/resources/db/migration/V20151008.1__t_data_realtime_report.sql
+++ src/main/resources/db/migration/V20151008.1__t_data_realtime_report.sql
@@ -36,6 +36,13 @@ CREATE TABLE IF NOT EXISTS t_data_realtime_report (
   INDEX idx_vin(vin),
   INDEX idx_imei(imei),
   INDEX idx_sending_time(sending_time),
-  INDEX idx_trip_id(trip_id)
+  INDEX idx_trip_id(trip_id),
+  INDEX idx_driving_time(driving_time),
+  INDEX idx_fuel_oil(fuel_oil),
+  INDEX idx_avg_oil_a(avg_oil_a),
+  INDEX idx_avg_oil_b(avg_oil_b),
+  INDEX idx_driving_range(driving_range),
+  INDEX idx_mileage_range(mileage_range),
+  INDEX idx_service_intervall(service_intervall)
 ) DEFAULT CHARSET=utf8 COMMENT='实时数据表';
 
