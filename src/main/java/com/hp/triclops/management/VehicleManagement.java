ed42063a5482ab6e578898e0f906b3ec17e3b518
TeemolSparrow <mtian@incardata.com.cn>
2016-02-02 15:12:59 +0800

    修改车辆管理方法

--- src/main/java/com/hp/triclops/management/VehicleManagement.java
+++ src/main/java/com/hp/triclops/management/VehicleManagement.java
@@ -1,9 +1,7 @@
 package com.hp.triclops.management;
 
-import com.hp.triclops.entity.UserEx;
 import com.hp.triclops.entity.VehicleEx;
 import com.hp.triclops.repository.VehicleExRepository;
-import com.hp.triclops.vo.UserExPartShow;
 import com.hp.triclops.vo.VehicleExShow;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -15,7 +13,6 @@ import org.springframework.stereotype.Component;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import java.util.stream.Collectors;
 
 /**
  * Created by Teemol on 2016/1/22.
@@ -25,13 +22,7 @@ import java.util.stream.Collectors;
 public class VehicleManagement {
 
     @Autowired
-    OrganizationUserManagement organizationUserManagement;
-
-    @Autowired
     OrganizationVehicleManagement organizationVehicleManagement;
-
-    @Autowired
-    UserVehicleManagement userVehicleManagement;
 
     @Autowired
     VehicleExRepository vehicleExRepository;
@@ -44,6 +35,8 @@ public class VehicleManagement {
     public VehicleExShow findById(int vid)
     {
         VehicleEx vehicle = vehicleExRepository.findById(vid);
+        if(vehicle == null)
+            return null;
         return new VehicleExShow(vehicle);
     }
 
@@ -161,87 +154,87 @@ public class VehicleManagement {
         return vehiclePage;
     }
 
-    /**
-     * 条件查询车辆
-     * @param oid 组织ID
-     * @param uid 用户ID
-     * @param vin 车架号
-     * @param tboxsn tbox码
-     * @param vendor 厂家
-     * @param model 型号
-     * @param start_date 生产日期开始范围
-     * @param end_date 生产日期结束范围
-     * @param license_plate 车牌号
-     * @param t_flag 是否可用 0：车辆未禁用 1：已禁用'
-     * @param currentPage 当前页
-     * @param pageSize 页面大小
-     * @return 车辆信息集合
-     */
-    public Page<VehicleEx> selectVehicle(Integer oid,int uid, String vin, String tboxsn, String vendor, String model, Date start_date,Date end_date,String license_plate,Integer t_flag, Integer currentPage,Integer pageSize)
-    {
-        if(vin!=null) vin = "%" + vin + "%";
-        if(tboxsn!=null) tboxsn = "%" + tboxsn + "%";
-        if(vendor!=null) vendor = "%" + vendor + "%";
-        if(model!=null) model = "%" + model + "%";
-        if(license_plate!=null) license_plate = "%" + license_plate + "%";
-        currentPage = currentPage==null?1:currentPage;
-        currentPage = currentPage<=0?1:currentPage;
-        pageSize = pageSize==null?10:pageSize;
-        pageSize = pageSize<=0?10:pageSize;
-        Pageable p = new PageRequest(currentPage-1,pageSize);
-
-        Page<VehicleEx> vehiclePage = new PageImpl<>(new ArrayList<>(),p,0);
-        List<Integer> vids = selectVehicleByUid(oid,uid);  // 查询用户有权查看的车辆ID集合
-        if(vids == null || vids.size()==0)
-        {
-            return vehiclePage;
-        }
-        vehiclePage = vehicleExRepository.selectVehicle(vids,vin,tboxsn,vendor,model,start_date,end_date,license_plate,t_flag,p);
-
-        return vehiclePage;
-    }
-
-    /**
-     * 查询用户有权查看的车辆ID集合
-     * @param oid 组织ID
-     * @param uid 用户ID
-     * @return 车辆ID集合
-     */
-    private List<Integer> selectVehicleByUid(Integer oid,int uid)
-    {
-        List<Integer> orgVids = new ArrayList<>();
-        List<Integer> oids = organizationUserManagement.findOidsByUid(uid);
-
-        if( oid!= null)
-        {
-            if(oids.contains(oid))
-            {
-                oids.clear();
-                oids.add(oid);
-            }
-            else
-            {
-                oids.clear();
-            }
-        }
-
-        if(oids.size()>0)
-        {
-            orgVids = organizationVehicleManagement.findVidByOids(oids);
-        }
-
-        if(oid == null)
-        {
-            List<Integer> ownerVids = userVehicleManagement.findVidByUid(uid);
-
-            for(Integer vid:ownerVids) {
-                if(!orgVids.contains(vid)){
-                    orgVids.add(vid);
-                }
-            }
-        }
-
-        return orgVids;
-    }
+//    /**
+//     * 条件查询车辆
+//     * @param oid 组织ID
+//     * @param uid 用户ID
+//     * @param vin 车架号
+//     * @param tboxsn tbox码
+//     * @param vendor 厂家
+//     * @param model 型号
+//     * @param start_date 生产日期开始范围
+//     * @param end_date 生产日期结束范围
+//     * @param license_plate 车牌号
+//     * @param t_flag 是否可用 0：车辆未禁用 1：已禁用'
+//     * @param currentPage 当前页
+//     * @param pageSize 页面大小
+//     * @return 车辆信息集合
+//     */
+//    public Page<VehicleEx> selectVehicle(Integer oid,int uid, String vin, String tboxsn, String vendor, String model, Date start_date,Date end_date,String license_plate,Integer t_flag, Integer currentPage,Integer pageSize)
+//    {
+//        if(vin!=null) vin = "%" + vin + "%";
+//        if(tboxsn!=null) tboxsn = "%" + tboxsn + "%";
+//        if(vendor!=null) vendor = "%" + vendor + "%";
+//        if(model!=null) model = "%" + model + "%";
+//        if(license_plate!=null) license_plate = "%" + license_plate + "%";
+//        currentPage = currentPage==null?1:currentPage;
+//        currentPage = currentPage<=0?1:currentPage;
+//        pageSize = pageSize==null?10:pageSize;
+//        pageSize = pageSize<=0?10:pageSize;
+//        Pageable p = new PageRequest(currentPage-1,pageSize);
+//
+//        Page<VehicleEx> vehiclePage = new PageImpl<>(new ArrayList<>(),p,0);
+//        List<Integer> vids = selectVehicleByUid(oid,uid);  // 查询用户有权查看的车辆ID集合
+//        if(vids == null || vids.size()==0)
+//        {
+//            return vehiclePage;
+//        }
+//        vehiclePage = vehicleExRepository.selectVehicle(vids,vin,tboxsn,vendor,model,start_date,end_date,license_plate,t_flag,p);
+//
+//        return vehiclePage;
+//    }
+//
+//    /**
+//     * 查询用户有权查看的车辆ID集合
+//     * @param oid 组织ID
+//     * @param uid 用户ID
+//     * @return 车辆ID集合
+//     */
+//    private List<Integer> selectVehicleByUid(Integer oid,int uid)
+//    {
+//        List<Integer> orgVids = new ArrayList<>();
+//        List<Integer> oids = organizationUserManagement.findOidsByUid(uid);
+//
+//        if( oid!= null)
+//        {
+//            if(oids.contains(oid))
+//            {
+//                oids.clear();
+//                oids.add(oid);
+//            }
+//            else
+//            {
+//                oids.clear();
+//            }
+//        }
+//
+//        if(oids.size()>0)
+//        {
+//            orgVids = organizationVehicleManagement.findVidByOids(oids);
+//        }
+//
+//        if(oid == null)
+//        {
+//            List<Integer> ownerVids = userVehicleManagement.findVidByUid(uid);
+//
+//            for(Integer vid:ownerVids) {
+//                if(!orgVids.contains(vid)){
+//                    orgVids.add(vid);
+//                }
+//            }
+//        }
+//
+//        return orgVids;
+//    }
 
 }
