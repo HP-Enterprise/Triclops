f1c18b3aec3b7631c7fd986c6590f7cacd367e5d
TeemolSparrow <mtian@incardata.com.cn>
2016-03-24 14:07:26 +0800

    完成TBOX管理方法

--- src/main/java/com/hp/triclops/repository/TBoxExRepository.java
+++ src/main/java/com/hp/triclops/repository/TBoxExRepository.java
@@ -1,10 +1,15 @@
 package com.hp.triclops.repository;
 
 import com.hp.triclops.entity.TBoxEx;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.repository.CrudRepository;
 
 /**
  * Created by Incar on 2016/3/24.
  */
 public interface TBoxExRepository  extends CrudRepository<TBoxEx, Long> {
+
+    Long deleteById(int id);
+
+    TBoxEx findById(int id);
 }
