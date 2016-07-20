0dc40164da740874dcb0afca5227ec621c2a56a6
Liujingguo91year <jgliu@incardata.com.cn>
2016-07-20 11:55:15 +0800

    修改

---
+++ src/main/java/com/hp/triclops/entity/Store4s.java
@@ -0,0 +1,124 @@
+package com.hp.triclops.entity;
+
+import javax.persistence.*;
+
+/**
+ * Created by Liujingguo91year on 2016/7/20.
+ */
+@Entity
+@Table(name = "t_fs_store")
+public class Store4s {
+    private int id;
+    private int vehicleType;
+    private String province;
+    private String city;
+    private String name;
+    private String address;
+    private String hotllineExclusive;
+    private String hotline24;
+
+    public Store4s() {
+    }
+
+    public Store4s(int id, int vehicleType, String province, String city, String name, String address, String hotllineExclusive, String hotline24) {
+        this.id = id;
+        this.vehicleType = vehicleType;
+        this.province = province;
+        this.city = city;
+        this.name = name;
+        this.address = address;
+        this.hotllineExclusive = hotllineExclusive;
+        this.hotline24 = hotline24;
+    }
+
+    public Store4s(int vehicleType, String province, String city, String name, String address, String hotllineExclusive, String hotline24) {
+        this.vehicleType = vehicleType;
+        this.province = province;
+        this.city = city;
+        this.name = name;
+        this.address = address;
+        this.hotllineExclusive = hotllineExclusive;
+        this.hotline24 = hotline24;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false, insertable = true, updatable = true)
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "vehicle_Type", nullable = false, insertable = true, updatable = true, length = 15)
+    public int getVehicleType() {
+        return vehicleType;
+    }
+
+    public void setVehicleType(int vehicleType) {
+        this.vehicleType = vehicleType;
+    }
+
+    @Basic
+    @Column(name = "province", nullable = false, insertable = true, updatable = true, length = 50)
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+
+    @Basic
+    @Column(name = "city", nullable = false, insertable = true, updatable = true, length = 50)
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    @Basic
+    @Column(name = "name", nullable = false, insertable = true, updatable = true, length = 50)
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Basic
+    @Column(name = "address", nullable = false, insertable = true, updatable = true, length = 50)
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Basic
+    @Column(name = "hotlline_Exclusive", nullable = false, insertable = true, updatable = true, length = 50)
+    public String getHotllineExclusive() {
+        return hotllineExclusive;
+    }
+
+    public void setHotllineExclusive(String hotllineExclusive) {
+        this.hotllineExclusive = hotllineExclusive;
+    }
+
+    @Basic
+    @Column(name = "hotline24", nullable = false, insertable = true, updatable = true, length = 50)
+    public String getHotline24() {
+        return hotline24;
+    }
+
+    public void setHotline24(String hotline24) {
+        this.hotline24 = hotline24;
+    }
+}
---
+++ src/main/java/com/hp/triclops/vo/Store4sShow.java
@@ -0,0 +1,84 @@
+package com.hp.triclops.vo;
+
+import com.hp.triclops.entity.Store4s;
+
+/**
+ * Created by Liujingguo91year on 2016/7/20.
+ */
+public class Store4sShow {
+    private int vehicleType;
+    private String province;
+    private String city;
+    private String name;
+    private String address;
+    private String hotllineExclusive;
+    private String hotline24;
+
+    public Store4sShow(Store4s store4s) {
+        this.vehicleType = store4s.getVehicleType();
+        this.province = store4s.getProvince();
+        this.city = store4s.getCity();
+        this.name = store4s.getName();
+        this.address = store4s.getAddress();
+        this.hotllineExclusive = store4s.getHotllineExclusive();
+        this.hotline24 = store4s.getHotline24();
+
+
+    }
+
+    public int getVehicleType() {
+        return vehicleType;
+    }
+
+    public void setVehicleType(int vehicleType) {
+        this.vehicleType = vehicleType;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getHotllineExclusive() {
+        return hotllineExclusive;
+    }
+
+    public void setHotllineExclusive(String hotllineExclusive) {
+        this.hotllineExclusive = hotllineExclusive;
+    }
+
+    public String getHotline24() {
+        return hotline24;
+    }
+
+    public void setHotline24(String hotline24) {
+        this.hotline24 = hotline24;
+    }
+}
