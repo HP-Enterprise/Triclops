23f2db2b079e250a3224f4920f95df6d81c5d3ad
Liujingguo91year <jgliu@incardata.com.cn>
2016-07-20 12:03:38 +0800

    添加

---
+++ src/main/java/com/hp/triclops/repository/FSStoreReposittory.java
@@ -0,0 +1,21 @@
+package com.hp.triclops.repository;
+
+import com.hp.triclops.entity.Shop4s;
+import com.hp.triclops.entity.Store4s;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import java.util.List;
+
+/**
+ * Created by Liujingguo91year on 2016/7/20.
+ */
+@EnableJpaRepositories
+public interface FSStoreReposittory extends JpaRepository<Store4s,Long> {
+    @Query("select s from Store4s s where " +
+            "(?1 is null or s.vehicleType = ?1)" +
+            " and (?2 is null or s.province = ?2)" +
+            " and (?3 is null or s.city  = ?3)")
+    List<Store4s> findByKeys(Integer vehicleType, String province, String city);
+}
