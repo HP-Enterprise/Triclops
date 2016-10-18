6a21d2fc3488b12d346c9b67755a8290625aec45
jack-luj <wjsc@qq.com>
2016-10-18 17:51:36 +0800

    提醒消息

---
+++ src/main/resources/db/migration/V20161018.1__t_warning_message_conversion.sql
@@ -0,0 +1,66 @@
+delete from t_warning_message_conversion;
+insert into t_warning_message_conversion(message_id,message_en,message_zh,group_id,group_message) values('1','','','1','安全气囊系统故障');
+insert into t_warning_message_conversion(message_id,message_en,message_zh,group_id,group_message) values('2','','','1','安全气囊系统故障');
+
+insert into t_warning_message_conversion(message_id,message_en,message_zh,group_id,group_message) values('3','','','2','电子转向锁故障');
+
+insert into t_warning_message_conversion(message_id,message_en,message_zh,group_id,group_message) values('4','','','3','车身控制器故障');
+insert into t_warning_message_conversion(message_id,message_en,message_zh,group_id,group_message) values('5','','','3','车身控制器故障');
+
+insert into t_warning_message_conversion(message_id,message_en,message_zh,group_id,group_message) values('6','','','4','电子稳定系统故障');
+      insert into t_warning_message_conversion(message_id,message_en,message_zh,group_id,group_message) values('10','','','4','电子稳定系统故障');
+
+insert into t_warning_message_conversion(message_id,message_en,message_zh,group_id,group_message) values('11','','','5','制动系统故障');
+insert into t_warning_message_conversion(message_id,message_en,message_zh,group_id,group_message) values('12','','','5','制动系统故障');
+insert into t_warning_message_conversion(message_id,message_en,message_zh,group_id,group_message) values('105','','','5','制动系统故障');
+insert into t_warning_message_conversion(message_id,message_en,message_zh,group_id,group_message) values('120','','','5','制动系统故障');
+
+insert into t_warning_message_conversion(message_id,message_en,message_zh,group_id,group_message) values('18','','','6','变速器系统故障');
+insert into t_warning_message_conversion(message_id,message_en,message_zh,group_id,group_message) values('22','','','6','变速器系统故障');
+insert into t_warning_message_conversion(message_id,message_en,message_zh,group_id,group_message) values('96','','','6','变速器系统故障');
+insert into t_warning_message_conversion(message_id,message_en,message_zh,group_id,group_message) values('146','','','6','变速器系统故障');
+insert into t_warning_message_conversion(message_id,message_en,message_zh,group_id,group_message) values('148','','','6','变速器系统故障');
+
+insert into t_warning_message_conversion(message_id,message_en,message_zh,group_id,group_message) values('20','','','7','发动机系统故障');
+insert into t_warning_message_conversion(message_id,message_en,message_zh,group_id,group_message) values('21','','','7','发动机系统故障');
+insert into t_warning_message_conversion(message_id,message_en,message_zh,group_id,group_message) values('23','','','7','发动机系统故障');
+insert into t_warning_message_conversion(message_id,message_en,message_zh,group_id,group_message) values('95','','','7','发动机系统故障');
+insert into t_warning_message_conversion(message_id,message_en,message_zh,group_id,group_message) values('145','','','7','发动机系统故障');
+
+insert into t_warning_message_conversion(message_id,message_en,message_zh,group_id,group_message) values('9','','','8','刹车防抱死系统故障');
+insert into t_warning_message_conversion(message_id,message_en,message_zh,group_id,group_message) values('13','','','9','电动助力转向系统故障');
+
+insert into t_warning_message_conversion(message_id,message_en,message_zh,group_id,group_message) values('16','','','10','无钥匙进入系统故障');
+insert into t_warning_message_conversion(message_id,message_en,message_zh,group_id,group_message) values('61','','','10','无钥匙进入系统故障');
+
+insert into t_warning_message_conversion(message_id,message_en,message_zh,group_id,group_message) values('19','','','11','胎压监测系统故障');
+
+insert into t_warning_message_conversion(message_id,message_en,message_zh,group_id,group_message) values('59','','','12','倒车雷达系统故障');
+insert into t_warning_message_conversion(message_id,message_en,message_zh,group_id,group_message) values('50','','','12','倒车雷达系统故障');
+
+insert into t_warning_message_conversion(message_id,message_en,message_zh,group_id,group_message) values('51','','','13','盲区监测系统故障');
+
+insert into t_warning_message_conversion(message_id,message_en,message_zh,group_id,group_message) values('83','','','14','电子手刹系统故障');
+insert into t_warning_message_conversion(message_id,message_en,message_zh,group_id,group_message) values('118','','','14','电子手刹系统故障');
+insert into t_warning_message_conversion(message_id,message_en,message_zh,group_id,group_message) values('122','','','14','电子手刹系统故障');
+
+insert into t_warning_message_conversion(message_id,message_en,message_zh,group_id,group_message) values('87','','','15','陡坡缓降系统故障');
+
+insert into t_warning_message_conversion(message_id,message_en,message_zh,group_id,group_message) values('90','','','16','定速巡航系统故障');
+
+insert into t_warning_message_conversion(message_id,message_en,message_zh,group_id,group_message) values('91','','','17','智能启停系统故障');
+insert into t_warning_message_conversion(message_id,message_en,message_zh,group_id,group_message) values('92','','','17','智能启停系统故障');
+insert into t_warning_message_conversion(message_id,message_en,message_zh,group_id,group_message) values('93','','','17','智能启停系统故障');
+
+insert into t_warning_message_conversion(message_id,message_en,message_zh,group_id,group_message) values('97','','','18','蓄电池故障');
+
+insert into t_warning_message_conversion(message_id,message_en,message_zh,group_id,group_message) values('110','','','19','自动驻车系统故障');
+
+insert into t_warning_message_conversion(message_id,message_en,message_zh,group_id,group_message) values('121','','','20','上坡辅助系统故障');
+
+
+
+
+
+
+
