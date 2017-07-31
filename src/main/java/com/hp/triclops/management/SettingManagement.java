482a78f46868fd8978b6e77a090e83a4ba4be30b
sunjun1 <strange001@163.com>
2017-07-31 11:19:50 +0800

    参数SQL查询修改

---
+++ src/main/java/com/hp/triclops/management/SettingManagement.java
@@ -0,0 +1,159 @@
+package com.hp.triclops.management;
+
+import com.hp.triclops.entity.TBox;
+import com.hp.triclops.entity.TBoxEx;
+import com.hp.triclops.repository.TBoxExRepository;
+import com.hp.triclops.repository.TBoxRepositoryDAO;
+import com.hp.triclops.utils.Page2;
+import com.hp.triclops.vo.TBoxExShow;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by Incar on 2016/3/23.
+ */
+@Component
+public class SettingManagement {
+
+    @Autowired
+    TBoxExRepository tBoxExRepository;
+    @Autowired
+    TBoxRepositoryDAO tBoxRepositoryDao;
+
+    /**
+     * 保存TBox信息
+     * @param tBoxExShow TBox信息
+     * @return 保存后的TBox信息
+     */
+    public TBoxExShow saveTBox(TBoxExShow tBoxExShow)
+    {
+        TBoxEx tBoxEx = new TBoxEx(tBoxExShow);
+        TBoxEx returnTBoxEx = tBoxExRepository.save(tBoxEx);
+        return new TBoxExShow(returnTBoxEx);
+    }
+
+    /**
+     * 删除TBox
+     * @param id
+     */
+    public void deleteTBox(int id)
+    {
+        tBoxExRepository.deleteById(id);
+    }
+
+    /**
+     * 修改TBox信息
+     * @param tBoxExShow TBox信息
+     * @return 修改后的TBox信息
+     */
+    public TBoxExShow modifyTBox(TBoxExShow tBoxExShow)
+    {
+        TBoxEx tBoxEx = new TBoxEx(tBoxExShow);
+        TBoxEx returnTBoxEx = tBoxExRepository.save(tBoxEx);
+        return new TBoxExShow(returnTBoxEx);
+    }
+
+    /**
+     * 绑定TBox
+     * @param tBoxExShow TBox信息
+     */
+    public TBoxExShow bindTBox(TBoxExShow tBoxExShow)
+    {
+        return modifyTBox(tBoxExShow);
+    }
+
+    /**
+     * 解绑TBox
+     * @param id TBox ID
+     */
+    public void unbindTBox(int id)
+    {
+        TBoxExShow tBoxExShow = findById(id);
+        if(tBoxExShow!=null)
+        {
+            Integer vid = tBoxExShow.getVid();
+            if(vid!=null)
+            {
+                tBoxExShow.setVid(null);
+                tBoxExShow.setVin(null);
+                saveTBox(tBoxExShow);
+            }
+        }
+    }
+
+    /**
+     * 查询TBox
+     * @param id TBox ID
+     * @return TBox 信息
+     */
+    public TBoxExShow findById(int id)
+    {
+        TBoxEx tBoxEx = tBoxExRepository.findById(id);
+        if(tBoxEx == null)
+        {
+            return null;
+        }
+        return new TBoxExShow(tBoxEx);
+    }
+
+    /**
+     * 根据TBox码查询TBox信息
+     * @param t_sn TBox码
+     * @return TBox信息
+     */
+    public TBoxExShow findByT_sn(String t_sn)
+    {
+        TBoxEx tBoxEx = tBoxExRepository.findByT_sn(t_sn);
+        if(tBoxEx == null)
+        {
+            return null;
+        }
+        return new TBoxExShow(tBoxEx);
+    }
+
+    /**
+     * 根据TBox ID和车辆ID查询TBox信息
+     * @param id TBox ID
+     * @param vid 车辆ID
+     * @return TBox信息
+     */
+    public TBoxExShow findByIdAndVid(int id, int vid)
+    {
+        TBoxEx tBoxEx = tBoxExRepository.findByIdAndVid(id,vid);
+        if(tBoxEx==null)
+        {
+            return null;
+        }
+        return new TBoxExShow(tBoxEx);
+    }
+
+    /**
+     * 根据条件查询TBox列表
+     * @param id ID
+     * @param t_sn tobx码
+     * @param isbind 0 or null 查找全部 1 查找未绑定的
+     * @param vin VIN码
+     * @param isActivated 是否被激活
+     * @param imei IMEI
+     * @param mobile SIM卡
+     * @param total 是否查全部，不分页：0分页查询，1为查询全部
+     * @param orderByProperty 排序条件 TBox类的某一个属性,默认id
+     * @param ascOrDesc 排序顺序接受字符串 "ASC"或"DESC"  大小写均可,默认ASC
+     * @param fuzzy 查询类型标志 0 精确查询 1 模糊查询
+     * @param pageSize 每页大小
+     * @param currentPage 页码
+     * @return 分页对象
+     */
+    public Page2<TBox> findTboxByKeys(Integer id, String t_sn, Integer isbind, String vin, Integer isActivated, String imei, String mobile, Integer total, String orderByProperty, String ascOrDesc, Integer fuzzy, Integer pageSize, Integer currentPage){
+        currentPage = currentPage == null ? 1 : currentPage;
+        pageSize = pageSize == null ? 10 : pageSize;
+        id = id == null ? 0 : id;
+        isActivated = isActivated == null ? 0 : isActivated;
+        total = total == null ? 0 :total;
+        fuzzy = fuzzy == null ? 0 : fuzzy;
+        isbind = isbind == null ? 0 : isbind;
+        orderByProperty=(orderByProperty==null)?"id":orderByProperty;
+        ascOrDesc=(ascOrDesc==null)?"ASC":ascOrDesc;
+        return tBoxRepositoryDao.findTboxByKeys(id,t_sn, isbind, vin,isActivated,imei,mobile,total,orderByProperty,ascOrDesc,fuzzy,pageSize,currentPage);
+    }
+}
---
+++ src/main/java/com/hp/triclops/repository/SettingRepositoryDAO.java
@@ -0,0 +1,162 @@
+package com.hp.triclops.repository;
+
+import com.hp.triclops.entity.TBox;
+import com.hp.triclops.utils.Page2;
+import org.springframework.stereotype.Component;
+import javax.persistence.*;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by luj on 2015/8/26.
+ */
+@Component
+public class SettingRepositoryDAO {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    /**
+     * 根据条件查询TBox列表
+     * @param id ID
+     * @param t_sn tobx码
+     * @param isbind 0 or null 查找全部 1 查找未绑定的
+     * @param vin VIN码
+     * @param isActivated 是否被激活
+     * @param imei IMEI
+     * @param mobile SIM卡
+     * @param total 是否查全部，不分页：0分页查询，1为查询全部
+     * @param orderByProperty 排序条件 TBox类的某一个属性,默认id
+     * @param ascOrDesc 排序顺序接受字符串 "ASC"或"DESC"  大小写均可,默认ASC
+     * @param fuzzy 查询类型标志 0 精确查询 1 模糊查询
+     * @param pageSize 每页大小
+     * @param currentPage 页码
+     * @return 分页对象
+     */
+    public Page2<TBox> findTboxByKeys(int id, String t_sn,int isbind, String vin, int isActivated, String imei, String mobile,int total,String orderByProperty,String ascOrDesc, int fuzzy, int pageSize,int currentPage){
+        String jpql = "select b from TBox b where 1=1";
+        String jpqlSum = "select count(*) as count from t_tbox b where 1=1";
+        if(id != 0){
+            jpql += " and b.id = :id";
+            jpqlSum += " and b.id = :id";
+        }
+        if(isActivated != 0){
+            jpql += " and b.is_activated = :is_activated";
+            jpqlSum += " and b.is_activated = :is_activated";
+        }
+        if(isbind == 1){
+            jpql += " and b.vehicle is null";
+            jpqlSum += " and b.vid is null";
+        }
+        if(fuzzy == 0){ //精确查询
+            if(t_sn != null){
+                jpql += " and b.t_sn = :t_sn";
+                jpqlSum += " and b.t_sn = :t_sn";
+            }
+            if(vin != null){
+                jpql += " and b.vin = :vin";
+                jpqlSum += " and b.vin = :vin";
+            }
+            if(imei != null){
+                jpql += " and b.imei = :imei";
+                jpqlSum += " and b.imei = :imei";
+            }
+            if(mobile != null){
+                jpql += " and b.mobile = :mobile";
+                jpqlSum += " and b.mobile = :mobile";
+            }
+        }else{
+            if(t_sn != null){
+                jpql += " and b.t_sn like :t_sn";
+                jpqlSum += " and b.t_sn like :t_sn";
+            }
+            if(vin != null){
+                jpql += " and b.vin like :vin";
+                jpqlSum += " and b.vin like :vin";
+            }
+            if(imei != null){
+                jpql += " and b.imei like :imei";
+                jpqlSum += " and b.imei like :imei";
+            }
+            if(mobile != null){
+                jpql += " and b.mobile like :mobile";
+                jpqlSum += " and b.mobile like :mobile";
+            }
+        }
+
+        jpql=jpql+" Order by b."+orderByProperty+" "+ascOrDesc;
+        TypedQuery query = em.createQuery(jpql, TBox.class);
+        Query queryCount = em.createNativeQuery(jpqlSum);//EntityManager id closed
+        if(id != 0){
+            query.setParameter("id",id);
+            queryCount.setParameter("id",id);
+        }
+        if(isActivated != 0){
+            query.setParameter("is_activated",isActivated);
+            queryCount.setParameter("is_activated", isActivated);
+        }
+        if(fuzzy == 0){ //精确查询
+            if(t_sn != null){
+                query.setParameter("t_sn",t_sn);
+                queryCount.setParameter("t_sn",t_sn);
+            }
+            if(vin != null){
+                query.setParameter("vin",vin);
+                queryCount.setParameter("vin",vin);
+            }
+            if(imei != null){
+                query.setParameter("imei",imei);
+                queryCount.setParameter("imei",imei);
+            }
+            if(mobile != null){
+                query.setParameter("mobile",mobile);
+                queryCount.setParameter("mobile",mobile);
+            }
+        }else{ //模糊查询
+            if(t_sn != null){
+                query.setParameter("t_sn","%"+t_sn+"%");
+                queryCount.setParameter("t_sn","%"+t_sn+"%");
+            }
+            if(vin != null){
+                query.setParameter("vin","%"+vin+"%");
+                queryCount.setParameter("vin","%"+vin+"%");
+            }
+            if(imei != null){
+                query.setParameter("imei","%"+imei+"%");
+                queryCount.setParameter("imei","%"+imei+"%");
+            }
+            if(mobile != null){
+                query.setParameter("mobile","%"+mobile+"%");
+                queryCount.setParameter("mobile","%"+mobile+"%");
+            }
+        }
+        List items_sum =  queryCount.getResultList();
+        String count = items_sum.get(0).toString();
+        if(total == 0){
+            if(pageSize != 0  && currentPage != 0){
+                query.setFirstResult((currentPage - 1)* pageSize);
+                query.setMaxResults(pageSize);
+            }else{
+                currentPage = 1;
+                pageSize = Integer.valueOf(count);
+            }
+        }
+        List items = query.getResultList();
+        List<TBox> tBoxList = new ArrayList<TBox>();
+        for(Object obj : items){
+            TBox tBox = (TBox)obj;
+            tBoxList.add(tBox);
+        }
+
+        Page2<TBox> page2 = new Page2<TBox>();
+        page2.setCurrentPage(currentPage);
+        page2.setPageSize(pageSize);
+        page2.setRecordCount(Long.parseLong(count));
+        page2.setItems(tBoxList);
+
+        return page2;
+    }
+
+}
