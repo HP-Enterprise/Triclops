9dbb71a8084f4ce57667776c3515f2995ba2081b
baichangda <471267877@qq.com>
2018-01-30 11:26:53 +0800

    4s店增删改查

--- src/main/java/com/hp/triclops/repository/FSStoreReposittory.java
+++ src/main/java/com/hp/triclops/repository/FSStoreReposittory.java
@@ -3,6 +3,7 @@ package com.hp.triclops.repository;
 import com.hp.triclops.entity.Shop4s;
 import com.hp.triclops.entity.Store4s;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -12,7 +13,7 @@ import java.util.List;
  * Created by Liujingguo91year on 2016/7/20.
  */
 @EnableJpaRepositories
-public interface FSStoreReposittory extends JpaRepository<Store4s,Long> {
+public interface FSStoreReposittory extends JpaRepository<Store4s,Integer>,JpaSpecificationExecutor<Store4s> {
     @Query("select s from Store4s s where " +
             "(?1 is null or s.vehicleType = ?1)" +
             " and (?2 is null or s.province = ?2)" +
