8702f01132193baac24e7bca66b96d225f77fbc0
MEMORY\chenz <254483451@qq.com>
2018-06-13 15:11:04 +0800

    1.添加线程池配置

--- src/main/java/com/hp/triclops/SimpleConfig.java
+++ src/main/java/com/hp/triclops/SimpleConfig.java
@@ -1,18 +1,31 @@
 package com.hp.triclops;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.core.task.TaskExecutor;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @Configuration("com.hp.triclops.simpleConfig")
 @Import(com.hp.data.Application.class)
 public class SimpleConfig {
     @Autowired
     private RedisConnectionFactory redisConnectionFactory;
+
+    @Bean
+    @ConditionalOnMissingBean(TaskExecutor.class)
+    public TaskExecutor taskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(2);
+        executor.setMaxPoolSize(Runtime.getRuntime().availableProcessors() * 2);
+        executor.setQueueCapacity(512);
+        return executor;
+    }
 
     @Bean
     public RedisTemplate<String, Object> objectRedisTemplate(){
